0bf74e13e408a22ab5d24a8537958b35f4a535f0
Samyr Amiev <amyev7@gmail.com>
2023-04-28 05:09:37 +0700

    Edit parser_test.c

--- test/parser_test.c
+++ test/parser_test.c
@@ -4,7 +4,7 @@
 CTEST(ctest, check_perimetr)
 {
     float pi = 3.14;
-    float rad = 3.5;
+    float rad = 3.3;
     const double expected = 2 * pi * rad;
     const double result = percircle(rad);
     ASSERT_DBL_NEAR_TOL(expected, result, 1e-1);
@@ -13,7 +13,7 @@ CTEST(ctest, check_perimetr)
 CTEST(ctest, check_area)
 {
     float pi = 3.14;
-    float rad = 3.5;
+    float rad = 3.3;
     const double expected = pi * rad * rad;
     const double result = areacircle(rad);
     ASSERT_DBL_NEAR_TOL(expected, result, 1e-1);
